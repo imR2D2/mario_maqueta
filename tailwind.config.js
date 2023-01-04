e6ed08311f06d9611d932402a75394aa7242e9e0
imR2D2 <arturo.hzb14@gmail.com>
2023-01-04 05:50:34 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [
+        './public/**/*.html',
+    ],
+    darkmode:false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {
+
+        },
+    },
+    plugins: [],
+}
